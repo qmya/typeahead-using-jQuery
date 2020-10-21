cead8a1706ae5ff37c9d9c3b8cf2752790bd21d2
qmya <qandeelgul90@gmail.com>
2020-10-20 21:26:54 -0400

    change the link to get json

--- script/main.js
+++ script/main.js
@@ -1,17 +1,22 @@
-$(document).ready(function(){
-  $('#search').keyup(function(){
-    $('#result').html('');
-    var searchField = $('#search').val();
+$(document).ready(function () {
+  $("#search").keyup(function () {
+    $("#result").html("");
+    var searchField = $("#search").val();
     console.log(searchField);
-    var expression = new RegExp(searchField, 'i');
+    var expression = new RegExp(searchField, "i");
+
     console.log(expression);
-    $.getJSON('./jsonserver/db.json',function(data){
-      $.each(data, function(key,value){
-        if(value.name.search(expression) != -1)
-        {
-          $('#result').append('<li> value.name </li>');
+    $.getJSON("http://localhost:3000/states", function (data) {
+      $.each(data, function (key, value) {
+        if (value.name.search(expression) == 0) {
+          console.log("yes yes");
+          console.log(value);
+          console.log(value.name.search(expression));
+          $("#result").append(
+            "<option id='" + key + "'>" + value.name + "</option>"
+          );
         }
-      })
-    })
-  })
-})
+      });
+    });
+  });
+});
