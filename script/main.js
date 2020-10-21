3e811517e2f8b86e5fe22d5f5171e8c93d823dbe
qmya <qandeelgul90@gmail.com>
2020-10-20 21:31:22 -0400

    clean up the code and remove console

--- script/main.js
+++ script/main.js
@@ -2,16 +2,10 @@ $(document).ready(function () {
   $("#search").keyup(function () {
     $("#result").html("");
     var searchField = $("#search").val();
-    console.log(searchField);
     var expression = new RegExp(searchField, "i");
-
-    console.log(expression);
     $.getJSON("http://localhost:3000/states", function (data) {
       $.each(data, function (key, value) {
         if (value.name.search(expression) == 0) {
-          console.log("yes yes");
-          console.log(value);
-          console.log(value.name.search(expression));
           $("#result").append(
             "<option id='" + key + "'>" + value.name + "</option>"
           );
